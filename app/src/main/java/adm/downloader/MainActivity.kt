a85b275efb25ddf9826a464dc36b5f4beba51d2b
Ali Raza <itxaliiraza@gmail.com>
2025-02-10 03:34:19 +0500

    v new

--- app/src/main/java/adm/downloader/MainActivity.kt
+++ app/src/main/java/adm/downloader/MainActivity.kt
@@ -3,11 +3,6 @@ package com.down.adm_core
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.down.adm_core.ui.theme.AdmcoreTheme
 
 class MainActivity : ComponentActivity() {
 
@@ -19,18 +14,3 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AdmcoreTheme {
-        Greeting("Android")
-    }
-}
