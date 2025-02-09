5947773f33b9e5e66849a861a0ff7d000535e2af
Ali Raza <itxaliiraza@gmail.com>
2025-02-10 03:25:45 +0500

    v new

--- app/src/main/java/adm/downloader/MainActivity.kt
+++ app/src/main/java/adm/downloader/MainActivity.kt
@@ -3,7 +3,6 @@ package com.down.adm_core
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
